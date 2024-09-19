9059fe78ab5f54dd5480d3c5223f9c611d2a7669
John Joseph II <jackjoseph7447@gmail.com>
2024-09-19 12:05:35 -0400

    put in getCount

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,36 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context : Context, _array : Array<Int>) : BaseAdapter() {
+
+    val context = _context
+    val array = _array
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return array.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        //return array[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        //return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+//        val textView : TextView
+//
+//        textView = TextView(context)
+//        textView.textSize = array[position].toFloat()
+//        textView.setPadding(5,10,0,10)
+//
+//        return textView
     }
 
 }
